1341480a1189cb34b4bfb021dbe7166211a08610
shwetasharma1302 <126080003+shwetasharma1302@users.noreply.github.com>
2023-08-17 16:35:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function threeSum(arr, target) {
-// write your code here
-  
+    if (arr.length < 3) {
+        return null; // There are not enough elements to form a sum of 3
+    }
+
+    arr.sort((a, b) => a - b); // Sort the array in ascending order
+
+    let closestSum = arr[0] + arr[1] + arr[2];
+
+    for (let i = 0; i < arr.length - 2; i++) {
+        let left = i + 1;
+        let right = arr.length - 1;
+
+        while (left < right) {
+            const sum = arr[i] + arr[left] + arr[right];
+
+            if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                closestSum = sum;
+            }
+
+            if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return closestSum;
 }
 
 module.exports = threeSum;
+
